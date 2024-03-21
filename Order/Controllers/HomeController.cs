e4893419085249eeecc3d9f543d50676d896a79d
Hassan Koraim <hassankoraim2@gmail.com>
2024-03-21 21:30:51 +0200

    Route Order in HomeController

---
+++ Order/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ordersolution.Models;
+namespace Ordersolution.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/order")]
+        public IActionResult Index(Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                string ErrorMessage = string.Join("\n", ModelState.Values
+                                       .SelectMany(x => x.Errors)
+                                                          .Select(x => x.ErrorMessage));
+                return BadRequest(ErrorMessage);
+            }
+            else
+            {
+                return Json(order);
+            }
+        }
+    }
+}
--- Order/Models/Product.cs
+++ Order/Models/Product.cs
@@ -1,4 +1,4 @@
-﻿namespace Order.Models
+﻿namespace Ordersolution.Models
 {
     public class Product
     {
