63fbd0c30034b7a6ce9fd3c1e2a644ed9fbae7ca
start <start@HASSANKORAIM>
2024-03-21 20:43:09 +0200

    Add project files.

---
+++ Order/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Order.Models
+{
+    public class Order
+    {
+        public int? OrderNo { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public double InvoicePrice { get; set; }
+        public List<Product>? product { get; set; }
+    }
+}
---
+++ Order/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Order.Models
+{
+    public class Product
+    {
+        public int? ProductCode { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
