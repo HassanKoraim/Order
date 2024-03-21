ee0e81cfe4b0feece8b208469258fbc6c8ce6c1e
Hassan Koraim <hassankoraim2@gmail.com>
2024-03-21 21:20:52 +0200

    New Message

--- Order/Models/Order.cs
+++ Order/Models/Order.cs
@@ -5,6 +5,10 @@
         public int? OrderNo { get; set; }
         public DateTime? OrderDate { get; set; }
         public double InvoicePrice { get; set; }
-        public List<Product>? product { get; set; }
+        public List<Product>? product { get; set; } 
+        public override string ToString()
+        {
+            return $"OrderNo: {OrderNo}";
+        }
     }
 }
