35b129b1b3cf239bc4df3f13957ee348bf5a9608
Hassan Koraim <hassankoraim2@gmail.com>
2024-03-22 14:22:33 +0200

    set more requirments validation for properties

--- Order/Models/Order.cs
+++ Order/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using OrderSolution.CustomValidator;
+using System.ComponentModel.DataAnnotations;
 
 namespace OrderSolution.Models
 {
@@ -7,10 +8,15 @@ namespace OrderSolution.Models
     {
         [BindNever]
         public int? OrderNo { get; set; }
+        [Required(ErrorMessage = "{0} should be applied")]
+        [Display(Name = "Order Date")]
         [MinimumDateValidatorAttribute]
         public DateTime? OrderDate { get; set; }
+        [Required(ErrorMessage = "{0} should be applied")]
+        [Display(Name = "Invoice Price")]
         [InvoicePriceValidator]
         public double? InvoicePrice { get; set; }
+        [Required(ErrorMessage = "{0} should be applied")]
         public List<Products>? product { get; set; } 
         public override string ToString()
         {
--- Order/Models/Products.cs
+++ Order/Models/Products.cs
@@ -1,9 +1,15 @@
-﻿namespace OrderSolution.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderSolution.Models
 {
     public class Products
     {
+        [Required(ErrorMessage = "{0} should be applied")]
+        [Display(Name = "Product Code")]
         public int? ProductCode { get; set; }
+        [Required(ErrorMessage = "{0} should be applied")]
         public double? Price { get; set; }
+        [Required(ErrorMessage = "{0} should be applied")]
         public int? Quantity { get; set; }
     }
 }
